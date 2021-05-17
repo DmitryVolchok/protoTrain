dc597e5558b8e9af8fd8a44a3609594976558ad9
dzmitryvol4 <75065085+dzmitryvol4@users.noreply.github.com>
2021-05-17 15:06:25 +0300

    getter and setter

--- trainee.js
+++ trainee.js
@@ -11,5 +11,39 @@ let rabbit = {
   __proto__: animal,
 };
 
+let longEar = {
+  earLength: 10,
+  __proto__: rabbit,
+};
+
 rabbit.walk();
+longEar.walk();
+alert(longEar.jumps);
 // rabbit.__proto__ = animal;
+
+////--------------------------------////////////////
+// seter
+
+let user = {
+  name: "Dmitry",
+  surname: "Minchenok",
+
+  set fullName(value) {
+    [this.name, this.surname] = value.split(" ");
+  },
+
+  get fullName() {
+    return `${this.name} ${this.surname}`;
+  },
+};
+
+let admin = {
+  __proto__: user,
+  isAdmin: true,
+};
+
+alert(admin.fullName);
+
+admin.fullName = "Dmitry Volchok";
+alert(admin.name);
+alert(admin.surname);
